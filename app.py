feaa1ee30f029bac73082e1ad76443f8fda35ea0
unknown <ivanperezgaliza@gmail.com>
2023-04-13 15:44:19 +0200

    api funcionando con deteccion de guantes

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 from ultralytics import YOLO
-from fastapi import FastAPI
-app = FastAPI()
+from flask import Flask, request, jsonify, flash, redirect, url_for, render_template
+
+app = Flask(__name__)
 model = YOLO("yolov8_ppe.pt")
 
-#result = model.predict("1.jpg", save=True, show=True)
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.route('/')
+def home():
+    print("Hello World")
+    return jsonify("Hello World")
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file:
+            file.save(file.filename)
+            result = model.predict(file.filename, save=True, show=True)
+            return jsonify(result)
+        else:
+            return jsonify("No file found")
+    
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
+    
