2c125bf8b2402c086c072b490c7cd26314f807f1
Ubuntu <ubuntu@ip-172-31-27-125.ec2.internal>
2023-05-06 13:17:04 +0000

    ultima update

--- app_flask.py
+++ app_flask.py
@@ -1,10 +1,25 @@
 from ultralytics import YOLO
 from flask import Flask, request, jsonify, flash, redirect, url_for, render_template
 from flask_cors import CORS, cross_origin
+import base64
+from PIL import Image
+from io import BytesIO
+import json
+import shutil
 
 app = Flask(__name__)
 CORS(app)
 model = YOLO("yolov8_ppe.pt")
+
+try:
+    shutil.rmtree("runs/detect/predict")
+except:
+    pass
+
+def image_to_base64(image_path):
+    with open(image_path, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+        return encoded_string.decode('utf-8')
 
 @app.route('/')
 def home():
@@ -15,16 +30,22 @@ def home():
 def predict():
    # return jsonify(request.form)
     if request.method == 'POST':
-        return jsonify(request.json, request.files, request.form)
-        if content is None:
-            return jsonify("No file found")
-        else:
-            return jsonify(content)
-            file = request.files['file']
-            file.save(file.filename)
-            result = model.predict(file.filename, save=True, show=True)
-            return jsonify(result)
+        if 'image' in request.json:
+            imagen_base64 = request.json['image']
+            imagen_bytes = base64.b64decode(imagen_base64)
+            imagen = Image.open(BytesIO(imagen_bytes))
+            imagen.save("imagen.jpg")
+            #wreturn jsonify(imagen_base64)
+            if imagen is None:
+                return jsonify("No file found")
+            else:
+                model.predict("imagen.jpg", save=True )
+                image_path = "runs/detect/predict/imagen.jpg"
+                base64_image = image_to_base64(image_path)
+                return jsonify(base64_image)
         
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
-    #app.run(debug=True, port=8000, host="172.31.27.125")
+    #app.run(debug=True, port=8000)
+    app.run(debug=True, port=8000, host="172.31.27.125")
+
+
